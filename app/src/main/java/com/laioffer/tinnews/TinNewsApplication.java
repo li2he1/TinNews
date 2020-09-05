287d13f48b8f522e34ac16aef52b1615d888e999
li2he1 <peterli2he1@gmail.com>
2020-09-05 18:39:20 -0400

    Android 7 Local Database with Room

--- app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
+++ app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
@@ -2,18 +2,27 @@ package com.laioffer.tinnews;
 
 import android.app.Application;
 
+import androidx.room.Room;
+
 import com.ashokvarma.gander.Gander;
 import com.ashokvarma.gander.imdb.GanderIMDB;
 import com.facebook.stetho.Stetho;
+import com.laioffer.tinnews.database.TinNewsDatabase;
 
 public class TinNewsApplication extends Application {
-
+    private TinNewsDatabase database;
     @Override
     public void onCreate() {
         super.onCreate();
         Gander.setGanderStorage(GanderIMDB.getInstance());
         Stetho.initializeWithDefaults(this);
-        // TODO: new code here.
+        database = Room.databaseBuilder(this, TinNewsDatabase.class, "tinnews_db").build();
+
     }
+
+    public TinNewsDatabase getDatabase() {
+               return database;
+    }
+
 }
 
